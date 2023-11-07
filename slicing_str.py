dad348a6574e77cd32f1059b3d2a333036baeb6f
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-07 18:42:43 +0000

    Add files via upload

---
+++ slicing_str.py
@@ -0,0 +1,13 @@
+s= "monty python"
+print (s[0 :4])
+print (s[1 :5])
+print (s[  :5])
+print (s[1 : ])
+print (s[1 :20])
+print (s[ : ])
+a = "hello"
+c = a + "zino"
+d =a+" "+"yusra"
+print (a,"bob")
+print (c)
+print (d)
